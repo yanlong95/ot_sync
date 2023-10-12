492844454302baa93e136ded8cd0b970a8104552
yanlong95 <mayanlong950305@gmail.com>
2023-10-12 14:08:01 -0700

    sync

--- valid/valid_seq_os1_rewrite.py
+++ valid/valid_seq_os1_rewrite.py
@@ -18,7 +18,7 @@ from tools.utils.utils import *
 np.set_printoptions(threshold=sys.maxsize)
 
 
-def validation():
+def validation(amodel):
     # ===============================================================================
     # loading paths and parameters
     config_filename = '../config/config_os1_rewrite.yml'
@@ -32,11 +32,32 @@ def validation():
     valid_scans = config['data_root']['valid_scan_folder']
     valid_poses = config['data_root']['valid_scan_poses']
     ground_truth = config['data_root']['gt_valid_folder']
-
+    seq_num = config['valid_config']['valid_seqs']
     # ===============================================================================
 
-    pass
+    with torch.no_grad():
+        num_scans = len(valid_scans)
+        descriptors = np.zeros((num_scans, 256))
+
+        for i in range(num_scans):
+            # load a scan
+            current_batch = read_one_need_from_seq(valid_scans, str(i).zfill(6), seq_num)
+            current_batch = torch.cat((current_batch, current_batch), dim=0)                # no idea why, keep it now
+
+            # calculate descriptor
+            amodel.eval()
+            current_descriptor = amodel(current_batch)
+            descriptors[i, :] = current_descriptor[0, :].cpu().detach().numpy()
+
+        descriptors = descriptors.astype('float32sync')
+
+
+
+
+
 
 
 if __name__ == '__main__':
-    validation()
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    amodel = featureExtracter(channels=32, use_transformer=True).to(device)
+    validation(amodel)
