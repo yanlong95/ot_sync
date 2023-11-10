119d5ab49925158d4012b1ca0238ff9a69901384
yanlong95 <mayanlong950305@gmail.com>
2023-11-09 23:16:38 -0800

    create voronoi map for keyframe

--- test/no_test_keyframes.py
+++ test/no_test_keyframes.py
@@ -8,7 +8,7 @@ if p not in sys.path:
 from matplotlib import pyplot as plt
 import torch
 import numpy as np
-import scipy as sp
+from scipy.spatial import Voronoi, voronoi_plot_2d, Delaunay, transform
 # from modules.overlap_transformer import featureExtracter
 from modules.overlap_transformer_haomo import featureExtracter
 from tools.read_samples import read_one_need_from_seq
@@ -58,7 +58,7 @@ def calc_ground_truth(poses):
 
     for i in range(len(poses)):
         rotation_matrix = poses[i, :3, :3]
-        rotation = sp.spatial.transform.Rotation.from_matrix(rotation_matrix)
+        rotation = transform.Rotation.from_matrix(rotation_matrix)
         quaternion = rotation.as_quat()
         frame_qua[i, :] = quaternion
 
@@ -87,7 +87,7 @@ def calc_top_n(keyframe_poses, keyframe_descriptors, test_frame_poses, test_fram
 
     # initial searching
     nlist = 1
-    k = 7
+    k = 5
     dim_pose = 3
     dim_descriptor = 256
 
@@ -110,10 +110,11 @@ def calc_top_n(keyframe_poses, keyframe_descriptors, test_frame_poses, test_fram
         curr_frame_pose = test_frame_poses[curr_frame_idx, :]
         curr_frame_descriptor = test_frame_descriptors[curr_frame_idx, :]
 
+        # searching top n poses and descriptors
         D_pose, I_pose = index_poses.search(curr_frame_pose, k)
         D_descriptor, I_descriptor = index_descriptors.search(curr_frame_descriptor, k)
 
-        # search
+        #
 
 
 
@@ -134,17 +135,102 @@ def testHandler(keyframe_path, test_frame_path, weights_path, test_selection=1):
         keyframe_locs, _ = calc_ground_truth(keyframe_poses)
         test_frame_locs_full, _ = calc_ground_truth(test_frame_poses)
 
-        keyframe_descriptors = calc_descriptors(keyframe_images, amodel)
-        test_frame_descriptors_full = calc_descriptors(test_frame_images, amodel)
+        # keyframe_descriptors = calc_descriptors(keyframe_images, amodel)
+        # test_frame_descriptors_full = calc_descriptors(test_frame_images, amodel)
+        #
+        # # select 1 sample per test_selection samples, reduce the test size
+        # test_frame_locs = test_frame_locs_full[::test_selection]
+        # test_frame_descriptors = test_frame_descriptors_full[::test_selection]
 
-        # select 1 sample per test_selection samples, reduce the test size
-        test_frame_locs = test_frame_locs_full[::test_selection]
-        test_frame_descriptors = test_frame_descriptors_full[::test_selection]
+        # test 2d plot
+        # keyframe_poses_plot(test_frame_locs_full, keyframe_locs)
+
+        # test voronoi map
+        calc_voronoi_map(keyframe_locs)
 
 
+def calc_voronoi_map(keyframe_poses):
+    xy = keyframe_poses[:, :2]
+    voronoi = Voronoi(xy, incremental=True)
+
+    # voronoi map is unbounded, set boundary by adding corner points
+    x_min = min(xy[:, 0])
+    x_max = max(xy[:, 0])
+    y_min = min(xy[:, 1])
+    y_max = max(xy[:, 1])
+    x_center = (x_min + x_max) / 2
+    y_center = (y_min + y_max) / 2
+    x_size = (x_max - x_min) / 2
+    y_size = (y_max - y_min) / 2
+
+    lower_left = [x_center - 1.2 * x_size, y_center - 1.2 * y_size]
+    lower_right = [x_center + 1.2 * x_size, y_center - 1.2 * y_size]
+    upper_left = [x_center - 1.2 * x_size, y_center + 1.2 * y_size]
+    upper_right = [x_center + 1.2 * x_size, y_center + 1.2 * y_size]
+    boundary = np.array([lower_left, lower_right, upper_left, upper_right])
+    voronoi.add_points(boundary)
+
+    # center for each region
+    voronoi_centers = voronoi.points[:-len(boundary)]
+
+    # extract the vertices positions for each region
+    vertices = voronoi.vertices                 # vertices positions
+    regions = voronoi.regions                   # indices of vertices for each region
+    point_region = voronoi.point_region         # voronoi region (index of polyhedron)
+
+    voronoi_vertices_indices = [regions[point_region[i]] for i in range(len(point_region) - len(boundary))]
+    voronoi_vertices_poses = []                 # the vertices for all regions
+    delaunay_triangulation = []                 # create delaunay triangulation to determine if a point is inside region
+
+    for voronoi_vertex_index in voronoi_vertices_indices:
+        vertices_poses = [vertices[idx] for idx in voronoi_vertex_index if idx > -1]
+        voronoi_vertices_poses.append(vertices_poses)
+        delaunay_triangulation.append(Delaunay(vertices_poses))
+
+    # double check if the point lies within the region for all the regions of the map
+    for i in range(len(voronoi_vertices_poses)):
+        point = voronoi_centers[i]
+        vertex = np.array(voronoi_vertices_poses[i])
+        tri = delaunay_triangulation[i]
+
+        if tri.find_simplex(point) < 0:                     # True if the point lies outside the region
+            print(f'point {i} lies outside the region.')
+
+        fig = voronoi_plot_2d(voronoi)
+        plt.scatter(point[0], point[1], c='b', s=50, label='center')
+        plt.scatter(vertex[:, 0], vertex[:, 1], c='r', s=100, label='vertices')
+        plt.legend()
+        plt.show()
+
+    # fig = voronoi_plot_2d(voronoi)
+    # plt.show()
+
+    return delaunay_triangulation, voronoi_centers
 
 
+def keyframe_poses_plot(poses, keyframe_poses, dim=2):
+    if dim == 2:
+        x = poses[:, 0]
+        y = poses[:, 1]
 
+        x_keyframe = keyframe_poses[:, 0]
+        y_keyframe = keyframe_poses[:, 1]
+
+        fig, ax = plt.subplots()
+        ax.scatter(x, y, c='gold', s=2, label='Trajectory')
+        ax.scatter(x_keyframe, y_keyframe, c='blue', s=3, label='Keyframe')
+
+        plt.title('Position')
+        plt.xlabel('x')
+        plt.ylabel('y')
+        plt.legend()
+
+        plt.show()
+
+    elif dim == 3:
+        xyz = poses
+    else:
+        raise "Error: dimension must be 2 or 3."
 
 
 if __name__ == '__main__':
