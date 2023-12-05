541f27dbe632ea7507a6e665164b468b61ded2c0
Yanlong Ma <yanlong@ucla.edu>
2023-12-04 19:58:02 -0800

    Update GUDA and CPU Time

--- test/test_keyframes_time.py
+++ test/test_keyframes_time.py
@@ -17,10 +17,11 @@ import tqdm
 import cv2
 import open3d as o3d
 import time
+from matplotlib import pyplot as plt
 
 
-def read_image(image_path):
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+def read_image(image_path, device):
+    # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     depth_data = np.array(cv2.imread(image_path, cv2.IMREAD_GRAYSCALE))
     depth_data_tensor = torch.from_numpy(depth_data).type(torch.FloatTensor).to(device)
     depth_data_tensor = torch.unsqueeze(depth_data_tensor, dim=0)
@@ -50,13 +51,13 @@ def load_one_test_frame_npy(test_frame_path):
     return np.load(test_frame_path)
 
 
-def calc_descriptors(images, amodel):
+def calc_descriptors(images, amodel, device):
     n_keyframes = len(images)
     descriptors = np.zeros((n_keyframes, 256))
 
     with torch.no_grad():
         for i in tqdm.tqdm(range(n_keyframes)):
-            curr_batch = read_image(images[i])
+            curr_batch = read_image(images[i], device)
             curr_batch = torch.cat((curr_batch, curr_batch), dim=0)
 
             amodel.eval()
@@ -67,9 +68,8 @@ def calc_descriptors(images, amodel):
     return descriptors
 
 
-def testHandler(keyframe_path, test_frames_path, weights_path, top_n=5):
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    # device = torch.device('cpu')
+def testHandler(keyframe_path, test_frames_path, weights_path, top_n=5, device=None):
+    # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     amodel = featureExtracter(height=32, width=900, channels=1, use_transformer=True).to(device)
 
     with torch.no_grad():
@@ -83,7 +83,7 @@ def testHandler(keyframe_path, test_frames_path, weights_path, top_n=5):
         keyframe_images, keyframe_poses = load_keyframes(keyframe_path)
 
         print('calculating descriptors for keyframe ...')
-        keyframe_descriptors = calc_descriptors(keyframe_images, amodel)
+        keyframe_descriptors = calc_descriptors(keyframe_images, amodel, device)
 
         # initialize searching
         nlist = 1
@@ -101,6 +101,7 @@ def testHandler(keyframe_path, test_frames_path, weights_path, top_n=5):
         test_frame_paths = load_files(test_frames_path)
 
         # test time
+        ts = []
         t_total = 0
         t_min = 2 ** 63 - 1
         t_max = 0
@@ -109,14 +110,14 @@ def testHandler(keyframe_path, test_frames_path, weights_path, top_n=5):
         t_calc_descriptor = 0
         t_detach = 0
         t_search = 0
-        for test_frame_path in test_frame_paths:
+        for test_frame_path in tqdm.tqdm(test_frame_paths):
             t_start = time.time()
             # choose loading from pcd file or npy
             point_cloud = load_one_test_frame_npy(test_frame_path)
             # point_cloud = load_one_test_frame_pcd(test_frame_path)
 
             # checking dimension (x, y, z)
-            assert point_cloud.shape[1] == 3
+            assert point_cloud.shape[1] == 3 or point_cloud.shape[1] == 4
 
             t_load_data_start = time.time()
 
@@ -140,7 +141,6 @@ def testHandler(keyframe_path, test_frames_path, weights_path, top_n=5):
             curr_descriptor = curr_descriptor.astype('float32')
             curr_descriptor = curr_descriptor.reshape(1, -1)
 
-            # TODO: searching
             D, I = index.search(curr_descriptor, top_n)
             top_n_keyframes_indices = I[0]
             # top_n_keyframes_poses = keyframe_poses[top_n_keyframes_indices, :]
@@ -152,27 +152,36 @@ def testHandler(keyframe_path, test_frames_path, weights_path, top_n=5):
             t_min = min(t_min, t_end - t_start)
             t_max = max(t_max, t_end - t_start)
 
-            # TODO: check both cpu and gpu
             t_load_data += t_load_data_end - t_load_data_start
-            t_calc_descriptor += t_calc_descriptor_end - t_calc_descriptor_end
+            t_calc_descriptor += t_calc_descriptor_end - t_load_data_end
             t_detach += t_detach_end - t_calc_descriptor_end
             t_search += t_search_end - t_detach_end
 
-        print('\n\n')
-        print(f'Average searching time: {t_total / len(test_frame_paths)}.')
+            ts.append(t_end - t_start)
+
+        t_avg = t_total / len(test_frame_paths)
+        t_loading = t_load_data / len(test_frame_paths)
+        t_calculating = t_calc_descriptor / len(test_frame_paths)
+        t_detaching = t_detach / len(test_frame_paths)
+        t_searching = t_search / len(test_frame_paths)
+
+        print('\n')
+        print(f'Average searching time: {t_avg}.')
         print(f'Maximal searching time: {t_max}.')
         print(f'Minimal searching time: {t_min}.')
-        print('\n\n')
+        print('\n')
 
-        print(f'Loading time: {t_load_data / len(test_frame_paths)}.')
-        print(f'Calculating time: {t_calc_descriptor / len(test_frame_paths)}.')
-        print(f'Detaching time: {t_detach / len(test_frame_paths)}.')
-        print(f'Searching time: {t_search / len(test_frame_paths)}')
+        print(f'Loading time: {t_loading}.')
+        print(f'Calculating time: {t_calculating}.')
+        print(f'Detaching time: {t_detaching}.')
+        print(f'Searching time: {t_searching}.')
+
+        return t_avg, t_max, t_min, t_loading, t_calculating, t_detaching, t_searching, ts
 
 
 if __name__ == '__main__':
     # load config ================================================================
-    config_filename = '../config/config_os1_rewrite.yml'
+    config_filename = '../config/config_os1_rewrite_time.yml'
     config = yaml.safe_load(open(config_filename))
     test_seq = config["test_config"]["test_seqs"][0]
     test_keyframe_path = config["test_config"]["test_keyframes"]
@@ -182,4 +191,38 @@ if __name__ == '__main__':
     test_frames_path_pcd_folder = os.path.join(test_frames_path, 'pcd_files')
     test_frames_path_npy_folder = os.path.join(test_frames_path, 'npy_files')
     # ============================================================================
-    testHandler(test_keyframe_path, test_frames_path_pcd_folder, test_weights_path, top_n=5)
+    # testHandler(test_keyframe_path, test_frames_path_pcd_folder, test_weights_path, top_n=5)
+    device1 = torch.device('cuda')
+    device2 = torch.device('cpu')
+    t_avg_cuda, t_max_cuda, t_min_cuda, t_loading_cuda, t_calculating_cuda, t_detaching_cuda, t_searching_cuda, ts_cuda = (
+        testHandler(test_keyframe_path, test_frames_path_npy_folder, test_weights_path, top_n=5, device=device1))
+    t_avg_cpu, t_max_cpu, t_min_cpu, t_loading_cpu, t_calculating_cpu, t_detaching_cpu, t_searching_cpu, ts_cpu = (
+        testHandler(test_keyframe_path, test_frames_path_npy_folder, test_weights_path, top_n=5, device=device2))
+
+    x = ['cuda', 'cpu']
+    time_avg = np.array([t_avg_cuda, t_avg_cpu])
+    time_max = np.array([t_max_cuda, t_max_cpu])
+    time_min = np.array([t_min_cuda, t_min_cpu])
+    times = [np.array(ts_cuda), np.array(ts_cpu)]
+    time_loading = np.array([t_loading_cuda, t_loading_cpu])
+    time_calculating = np.array([t_calculating_cuda, t_calculating_cpu])
+    time_detaching = np.array([t_detaching_cuda, t_detaching_cpu])
+    time_searching = np.array([t_searching_cuda, t_searching_cpu])
+
+    fig1, ax1 = plt.subplots()
+    ax1.boxplot(times, showfliers=False)
+    ax1.set_xticks(range(1, len(x) + 1))
+    ax1.set_xticklabels(x)
+    ax1.set_ylabel('Sec')
+    ax1.set_title('Time')
+
+    fig2, ax2 = plt.subplots()
+    ax2.bar(x, time_loading, label='Loading')                                                                # loading data and project
+    ax2.bar(x, time_calculating, bottom=time_loading, label='Calculating')                                   # calculating descriptors
+    ax2.bar(x, time_detaching, bottom=time_calculating+time_loading, label='Detaching')                      # detaching from gpu
+    ax2.bar(x, time_searching, bottom=time_detaching+time_calculating+time_loading, label='Searching')       # searching for keyframes
+    ax2.set_ylabel('Sec')
+    ax2.set_title('Time')
+    ax2.legend()
+    plt.show()
+
